281d04745a4244a1b9274a61a68a59a3cedebfd7
Maher Fared <maherfared@gmail.com>
2024-10-26 13:43:45 +0300

    first commit

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -34,13 +34,13 @@ class OrderController extends Controller
         $currency = "EGP"; //your currency
 
         $kashierOrderHash = $this->generateKashierOrderHash($order,$currency);
-        $paymentUrl = "https://checkout.kashier.io/?merchantId=MID-29264-164" .
+        $paymentUrl = "https://checkout.kashier.io/?merchantId=you-MID" .
             "&mode=test" .
             "&orderId={$order->id}" .
             "&amount={$order->price}" .
             "&currency={$currency}" .
             "&hash={$kashierOrderHash}" .
-            "&allowedMethods=card,bank_installments,wallet,fawry" . 
+            "&allowedMethods=card" . 
             "&merchantRedirect=" . urlencode('http://localhost:8000/callback') .
             "&failureRedirect=" . urlencode('http://localhost:8000/failure') .
             "&redirectMethod=get" .
@@ -51,10 +51,10 @@ class OrderController extends Controller
     
 
     private function generateKashierOrderHash($order,$currency){
-        $mid = "your-MID-here"; //your merchant id
+        $mid = "you-MID"; //your merchant id
         $amount = $order->price; //eg: 100
         $currency = $currency;
-        $orderId = $order->merchantOrderId; //eg: 99, your system order ID
+        $orderId = $order->id; //eg: 99, your system order ID
         $secret = "your-api-key";
         $path = "/?payment=".$mid.".".$orderId.".".$amount.".".$currency.(isset( $CustomerReference) ?(".".$CustomerReference):null);
         $hash = hash_hmac( 'sha256' , $path , $secret ,false);
@@ -64,7 +64,7 @@ class OrderController extends Controller
     public function handleCallback(Request $request)
     {
         // Define your secret API key
-        $secret = '04d5f792-d1b9-468f-881f-b66212303b75';
+        $secret = 'your-api-key';
 
         // Log the incoming request
         Log::info('Callback hit with parameters: ', $request->all());
